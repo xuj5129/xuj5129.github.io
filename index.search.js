432a2de6a0164028eecb1b92071ba652cc701785
xuj5129 <xuj5129@163.com>
2024-06-26 22:54:35 +0800

    1

---
+++ index.search.js
@@ -0,0 +1,42 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "攻略",
+    "uri": "/guide/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "工具类",
+    "uri": "/utils/index.html"
+  },
+  {
+    "breadcrumb": "工具类",
+    "content": "\r\u003c!DOCTYPE html\u003e\r仙境传说中央实验室密码\r十进制(1~255)\r二进制",
+    "description": "\r\u003c!DOCTYPE html\u003e\r仙境传说中央实验室密码\r十进制(1~255)\r二进制",
+    "tags": [],
+    "title": "中央实验室",
+    "uri": "/utils/centercode/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
